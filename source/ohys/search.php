3f36f2dd572a81e75797f3448ca7c93b9001e447
Alanimdeo <alan@imdeo.kr>
2021-09-06 02:01:12 +0900

    Update 1.1
    75개 이상 검색 지원

--- source/ohys/search.php
+++ source/ohys/search.php
@@ -1,6 +1,37 @@
 <?php
 include('simple_html_dom.php');
 
+function search($html, $count, $plugin) {
+	foreach ($html->find('tr[class="default"]') as $element) {
+		$title = $element->find('a[title]', -1)->innertext; // title
+		$download = $element->find('td', 2)->find('a', 1)->href; // download
+		if (strpos($element->find('td', -5)->innertext, 'GiB')) {
+			$multiple = 1073741824;
+		} else {
+			$multiple = 1048576;
+		}
+		$size = ((float)substr($element->find('td', -5)->innertext, 0, -4)) * $multiple;
+		$datetime = ($element->find('td[data-timestamp]', 0)->innertext).":00"; // datetime
+		$page = "https://nyaa.si".$element->find('a[title]', -1)->href; // page
+		$hash = md5($title);
+		$seeds = $element->find('td', -3)->innertext; // seeds
+		$leechs = $element->find('td', -2)->innertext; // leechs
+		$category = $element->find('a[title]', 0)->title; // category
+		$plugin->addResult($title, $download, $size, $datetime, $page, $hash, $seeds, $leechs, $category);
+		$count++;
+	}
+	if (count($html->find('ul[class="pagination"]')) === 1) {
+        if (count($html->find('ul[class="pagination"]', 0)->find('li[class="next disabled"]')) === 0) {
+            $html = file_get_html("https://nyaa.si".$html->find('li[class="next"]', 0)->find('a', 0)->href);
+            search($html, $count, $plugin);
+        } else {
+            return $count;
+        }
+    } else {
+		return $count;
+    }
+}
+
 class SynoDLMSearchOhys {
 	public function __construct() {
 	}
@@ -16,26 +47,7 @@ class SynoDLMSearchOhys {
 
 	public function parse($plugin, $response) {
 		$html = str_get_html($response);
-		$count = 0;
-		foreach ($html->find('tr[class="default"]') as $element) {
-			$title = $element->find('a[title]', -1)->innertext; // title
-			$download = $element->find('td', 2)->find('a', 1)->href; // download
-			if (strpos($element->find('td', -5)->innertext, 'GiB')) {
-				$multiple = 1073741824;
-			} else {
-				$multiple = 1048576;
-			}
-			$size = ((float)substr($element->find('td', -5)->innertext, 0, -4)) * $multiple;
-			$datetime = ($element->find('td[data-timestamp]', 0)->innertext).":00"; // datetime
-			$page = "https://nyaa.si".$element->find('a[title]', -1)->href; // page
-			$hash = md5($title);
-			$seeds = $element->find('td', -3)->innertext; // seeds
-			$leechs = $element->find('td', -2)->innertext; // leechs
-			$category = $element->find('a[title]', 0)->title; // category
-			$plugin->addResult($title, $download, $size, $datetime, $page, $hash, $seeds, $leechs, $category);
-			$count++;
-		}
-		return $count;
+		return search($html, 0, $plugin);
 	}
 }
 ?>
